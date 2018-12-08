f76b3fe0043157f966e1355f46cf015c2869aff0
Vladimir Rusch <vladimir.rusch@mallgroup.com>
2018-12-08 23:03:23 +0100

    initial

---
+++ application/controllers/edit.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlado
+ * Date: 08.12.2018
+ * Time: 22:52
+ */
+//todo: doplnit logiku
---
+++ application/views/edit/article.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlado
+ * Date: 08.12.2018
+ * Time: 22:50
+ */
+//todo: doplnit pohlad
