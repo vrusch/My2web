f678548f9b13e0f720f14f853288691abbc0278c
Vladimir Rusch <vladimir.rusch@mallgroup.com>
2018-12-09 18:04:03 +0100

    initial

---
+++ application/controllers/course.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlado
+ * Date: 08.12.2018
+ * Time: 22:52
+ */
+//todo: doplnit logiku
+
+class course extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		// Load the necessary stuff...
+		$this->load->helper(array('language', 'url', 'form', 'account/ssl'));
+		$this->load->library(array('account/authentication', 'account/authorization'));
+		$this->load->model(array('account/account_model'));
+	}
+
+	public function play($page = 'playcourse')
+	{
+		maintain_ssl();
+
+		if ($this->authentication->is_signed_in())
+		{
+			$data['account'] = $this->account_model->get_by_id($this->session->userdata('account_id'));
+		}
+
+		if ( ! file_exists(APPPATH.'views/course/'.$page.'.php'))
+		{
+			// Whoops, we don't have a page for that!
+			show_404();
+		}
+
+		$data['title'] = ucfirst($page); // Capitalize the first letter
+
+		$this->load->view('course/'.$page, isset($data) ? $data : NULL);
+	}
+}
