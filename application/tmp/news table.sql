75c6fe4526cd44ef3837cd4632f12b758df9cd80
Vladimir Rusch <vladimir.rusch@mallgroup.com>
2018-12-11 23:22:02 +0100

    sql pro zrizeni news tabulky

---
+++ application/tmp/news table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE news (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        title varchar(128) NOT NULL,
+        slug varchar(128) NOT NULL,
+        text text NOT NULL,
+        PRIMARY KEY (id),
+        KEY slug (slug)
+);
+
+ date_publish       timestamp         CURRENT_TIMESTAMP
+ lifetime           int(10)            30
